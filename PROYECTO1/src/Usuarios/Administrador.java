a828e4b35ee7c387fd42a76c9a15d7514309f014
MustyDrag7 <m.cucaita@uniandes.edu.co>
2025-04-13 22:40:26 -0500

    Administrador final

--- PROYECTO1/src/Usuarios/Administrador.java
+++ PROYECTO1/src/Usuarios/Administrador.java
@@ -1,25 +1,24 @@
 package Usuarios;
 
 import java.util.List;
+import Atracciones.Atraccion;
+import Servicios.LugarServicio;
 
 public class Administrador extends Usuario {
     private List<String> permisos;
 
-    public Administrador(String nombre, int id, List<String> permisos, String login, String password) {
+    public Administrador(String nombre, int id, List<String> permisos, 
+                        String login, String password) {
         super(nombre, id, login, password);
         this.permisos = permisos;
     }
 
-    public void gestionarAtracciones() {
-        // Me falta ver como hacen la logica de atracciones y las otras para hacer esta parte
+    public void gestionarAtraccion(Atraccion atraccion, boolean activar) {
+        atraccion.estado = activar;
     }
 
-    public void gestionarEmpleados() {
-        // Me falta ver como hacen la logica de atracciones y las otras para hacer esta parte
-    }
-
-    public void gestionarTiquetes() {
-        // Me falta ver como hacen la logica de atracciones y las otras para hacer esta parte
+    public void asignarEmpleado(Empleado empleado, LugarServicio lugar) {
+        lugar.asignarEmpleado(empleado);
     }
 
     public boolean tienePermiso(String permiso) {
